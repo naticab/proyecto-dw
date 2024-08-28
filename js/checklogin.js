355a25a82f245b881cfd16b559d5a9e7b1418ec1
marianasolaro <solaromariana@gmail.com>
2024-08-28 16:31:11 -0300

    mostrar usuario: index y checklogin

--- js/checklogin.js
+++ js/checklogin.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const isAuthenticated = localStorage.getItem('isAuthenticated');
-    if (!isAuthenticated) {
+    const username = localStorage.getItem('username');
+
+    const userContainer = document.getElementById('user-container');
+    const userNameElement = document.getElementById('user-name');
+    const loginLink = document.getElementById('loginLink');
+
+    if (isAuthenticated !== 'true') {
         window.location.href = 'login.html';
+        return;
     }
-});
+
+    if (isAuthenticated === 'true' && username) {
+        userNameElement.textContent = `${username}`;
+        userContainer.classList.remove('d-none');
+        loginLink.classList.add('d-none');
+    } else {
+        userContainer.classList.add('d-none');
+        loginLink.classList.remove('d-none');
+    }
+}); 
