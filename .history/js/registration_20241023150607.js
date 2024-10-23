16035713552239473114495c43980662e30ede6a
Barbara Bassin <53198802+romancegirl@users.noreply.github.com>
2024-10-23 15:33:53 -0300

    Corregir error 405

--- .history/js/registration_20241023150607.js
+++ .history/js/registration_20241023150607.js
@@ -1,30 +1,22 @@
 document.getElementById('togglePassword').addEventListener('click', function () {
-  const passwordField = document.getElementById('password');
-  const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordField.setAttribute('type', type);
-
-  this.classList.toggle('fa-eye-slash');
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    
+    this.classList.toggle('fa-eye-slash');
 });
 
-document.getElementById('myForm').addEventListener('submit', function (event) {
-  // Evitar que el formulario se envíe al servidor
-  event.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  if (username !== '' && password !== '') {
-      alert('¡Bienvenido! Muchas gracias por registrarte con nosotros');
-
-      localStorage.setItem('isAuthenticated', 'true');
-      localStorage.setItem('username', username);
-
-      window.location.href = 'index.html';
-  } else {
-      alert('Por favor, ingresa un usuario y contraseña válidos.');
-  }
+document.getElementById('button').addEventListener('click', function(event) {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (username !== '' && password !== '') {
+         localStorage.setItem('isAuthenticated', 'true');
+         localStorage.setItem('username', username);
+        window.location.href = 'index.html';
+    } else {
+         alert('Usuario o contraseña incorrectos.');
+     }
 });
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
