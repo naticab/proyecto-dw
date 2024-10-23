bfecbaea97f8d9e7644c62411db34b3785d2438a
Barbara Bassin <53198802+romancegirl@users.noreply.github.com>
2024-10-22 22:48:58 -0300

    Pantalla de registro

---
+++ .history/js/registration_20241022215017.js
@@ -0,0 +1,21 @@
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    
+    this.classList.toggle('fa-eye-slash');
+});
+
+document.getElementById('button').addEventListener('click', function(event) {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (username !== '' && password !== '') {
+         localStorage.setItem('isAuthenticated', 'true');
+         localStorage.setItem('username', username);
+        window.location.href = 'index.html';
+    } else {
+         alert('Usuario o contraseña incorrectos.');
+     }
+});
+
+
